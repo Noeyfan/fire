6561f3f8e627e8526d9d56a82894f2e4a4561f2b
fanyou <youfan.noey@gmail.com>
2021-08-29 21:34:42 -0700

    Fix text tuncate

--- src/Fire.tsx
+++ src/Fire.tsx
@@ -8,14 +8,14 @@ class Fire extends React.Component {
   private static INVEST_YOY = 0.06;
   private static HOUSE_YOY = 0.035;
   private static TAX = 0.3;
-  private static HOUSE_DOWN_PAY = 0.2;
 
   private static DEFAULT_FORM = {
+    years: 30,
     spending: 1000,
     mortgage: 1000,
     salary: 5000,
     hvalue: 470000,
-    funds: 300000
+    funds: 30000
   }
 
   constructor(props: any) {
@@ -23,17 +23,8 @@ class Fire extends React.Component {
     this.handleChange = this.handleChange.bind(this);
 
     this.state = {
+      ...this.buildAllGraphs(Fire.DEFAULT_FORM),
       form: Fire.DEFAULT_FORM,
-      cashFlowGraph: this.buildGraph(
-        Fire.DEFAULT_FORM,
-        (form) => this.calculateCashflow(form),
-        'Cash Flow Needed'
-      ),
-      networthGraph: this.buildGraph(
-        Fire.DEFAULT_FORM,
-        (form) => this.calculateNetworth(form),
-        'Net Worth'
-      ),
     };
   }
 
@@ -43,46 +34,70 @@ class Fire extends React.Component {
       [event.target.name]: parseInt(event.target.value),
     };
     this.setState({
+      ...this.buildAllGraphs(newForm),
       form: newForm,
-      cashFlowGraph: this.buildGraph(
-        newForm,
-        (form) => this.calculateCashflow(form),
-        'Cash Flow Needed'
-      ),
-      networthGraph: this.buildGraph(
-        newForm,
-        (form) => this.calculateNetworth(form),
-        'Net Worth'
-      )
     });
   }
 
-  buildxaxis() {
+  getState() {
+    return this.state as any;
+  }
+
+  buildxaxis(form: any) {
     let year = (new Date()).getFullYear();
     const result = [];
-    for (let i = 0; i <= 30; ++i) {
+    for (let i = 0; i <= form.years; ++i) {
       result.push(year + i);
     }
     return result;
   }
 
-  buildGraph(form: any, builder: (form: any) => any, title: string) {
+  buildAllGraphs(form: any) {
+    return {
+      cashFlowGraph: this.buildGraph(
+        form,
+        (form) => this.calculateCashflow(form),
+        'Cash Flow Needed',
+        'Lower means closer to financial freedom'
+      ),
+      networthGraph: this.buildGraph(
+        form,
+        (form) => this.calculateNetworth(form),
+        'Net Worth',
+        ''
+      )
+    };
+  }
+
+  buildGraph(
+    form: any,
+    builder: (form: any) => any,
+    title: string,
+    subtitle: string,
+  ) {
     const graph = {
       title: {
         text: title,
-      },
-      grid: {
-        containerLabel: true,
+        subtext: subtitle,
+        left: 'center'
       },
       xAxis: {
         type: 'category',
-        data: this.buildxaxis()
+        data: this.buildxaxis(form)
       },
       yAxis: [
         {
           type: 'value',
           axisLabel: {
-            formatter: '{value}k'
+            formatter: (value: number) => {
+              if (value >= 1000000) {
+                return value/1000000 + 'm';
+              } else if (value >= 1000){
+                return value/1000 + 'k';
+              } else {
+                return value;
+              }
+            }
           },
         }
       ],
@@ -110,12 +125,12 @@ class Fire extends React.Component {
     const yearlySpending = (form.spending + form.mortgage) * 12;
     const yearlyContribution = form.salary * 12 - yearlySpending;
     const result = [];
-    for (let i = 0; i <= Fire.YEARS; i++) {
+    for (let i = 0; i <= form.years; i++) {
       investmentGain = investmentFunds * (Fire.INVEST_YOY * (1 - Fire.TAX));
       investmentFunds += investmentGain + yearlyContribution;
       const cashFlow = investmentGain - yearlySpending;
       if (cashFlow <= 0) {
-        result.push(Math.round(-cashFlow/1000));
+        result.push(Math.round(-cashFlow));
       }
     }
 
@@ -130,12 +145,12 @@ class Fire extends React.Component {
     const yearlySpending = (form.spending + form.mortgage) * 12;
     const yearlyContribution = form.salary * 12 - yearlySpending;
     const result = [];
-    for (let i = 0; i <= Fire.YEARS; i++) {
+    for (let i = 0; i <= form.years; i++) {
       investmentGain = investmentFunds * (Fire.INVEST_YOY * (1 - Fire.TAX));
       houseFunds += houseFunds * Fire.HOUSE_YOY;
       investmentFunds += investmentGain + yearlyContribution;
       totalMortgage -= form.mortgage * 12;
-      result.push(Math.round((investmentFunds + houseFunds - totalMortgage)/1000));
+      result.push(Math.round(investmentFunds + houseFunds - totalMortgage));
     }
 
     return result;
@@ -152,21 +167,33 @@ class Fire extends React.Component {
     );
   }
 
+  addSlider(label: string, defaultValue: any, name: string) {
+    return (
+      <Col>
+        <Form.Group>
+          <Form.Label>{label}</Form.Label>
+          <Form.Range defaultValue={defaultValue} name={name}/>
+        </Form.Group>
+      </Col>
+    );
+  }
+
   render() {
     return (
       <Container>
-        <ReactECharts option={(this.state as any).cashFlowGraph} />
-        <ReactECharts option={(this.state as any).networthGraph} />
+        <ReactECharts option={this.getState().cashFlowGraph} />
+        <ReactECharts option={this.getState().networthGraph} />
         <Form onChange={this.handleChange}>
           <Row>
+          {this.addSlider(`Calculate for ${this.getState().form.years} years`, Fire.DEFAULT_FORM.years, 'years')}
           {this.addForm('Spending (per month)', Fire.DEFAULT_FORM.spending, 'spending')}
+          </Row>
+          <Row>
           {this.addForm('Mortgage (per month)', Fire.DEFAULT_FORM.mortgage, 'mortgage')}
-          </Row>
-          <Row>
           {this.addForm('Salary (per month)', Fire.DEFAULT_FORM.salary, 'salary')}
-          {this.addForm('House Value (total market value)', Fire.DEFAULT_FORM.hvalue, 'hvalue')}
           </Row>
           <Row>
+          {this.addForm('House Value', Fire.DEFAULT_FORM.hvalue, 'hvalue')}
           {this.addForm('Investment Starting fund', Fire.DEFAULT_FORM.funds, 'funds')}
           </Row>
           <Form.Text className="text-muted">
